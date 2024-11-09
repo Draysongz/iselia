f0241f9e4774868deb8777b9186388dd55e4ccc8
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-09 15:24:05 +0100

    doem

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,11 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import App from "./App.tsx";
 import './index.css';
 
+const manifestUrl ="https://raw.githubusercontent.com/draysongz/iselia/main/public/manifest.json";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TonConnectUIProvider manifestUrl="">
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
       <UserProvider>
         <ChakraProvider>
           <App />
