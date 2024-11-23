01e9e3045f7a6e2faf4d1de9fc4ffd659ae6f28a
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-23 19:37:40 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import Team from "./pages/Team";
 import Earn from "./pages/Earn";
 import Task from "./pages/Task";
 import Settings from "./pages/Settings";
-// import WebApp from "@twa-dev/sdk";
-// import { useEffect, useState } from "react";
+import WebApp from "@twa-dev/sdk";
+import { useEffect, useState } from "react";
 import { useUserLogin } from "./hooks/useAuth";
 import Loading from "./pages/Loading";
 
@@ -21,20 +21,20 @@ import { useUser } from "./context/context";
 import { useEffect } from "react";
 
 function App() {
-  //  const [initData, setInitData] = useState("");
+   const [initData, setInitData] = useState("");
   
    const {setToken, setUser} = useUser()
 
-   const initData ="query_id=AAElBO5_AAAAACUE7n8449Rf&user=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1731079459&hash=f976c3c9b1f46c418b33ddbb16f594986af8b32faa71c37a6c05de64442bee77";
+  //  const initData ="query_id=AAElBO5_AAAAACUE7n8449Rf&user=%7B%22id%22%3A2146305061%2C%22first_name%22%3A%22Crypto%22%2C%22last_name%22%3A%22Dray%22%2C%22username%22%3A%22Habibilord%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1731079459&hash=f976c3c9b1f46c418b33ddbb16f594986af8b32faa71c37a6c05de64442bee77";
    
 
-  //  useEffect(() => {
-  //   WebApp.expand()
-  //    const data = WebApp.initData;
-  //    setInitData(data);
+   useEffect(() => {
+    WebApp.expand()
+     const data = WebApp.initData;
+     setInitData(data);
 
  
-  //  }, []);
+   }, []);
 
   
 
